30341be613b1f14a290a64b63d8db67e9581af81
shehaodan <shehaodan@meituan.com>
2019-07-29 15:04:25 +0800

    change-some-word

--- array-length.sh
+++ array-length.sh
@@ -3,7 +3,7 @@
 nums=(29 100 13)
 echo ${#nums[*]}
 #向数组中添加元素
-nums[10]="http://www.dianping.com/"
+nums[10]="http://www.xxxxxxxx.com/"
 echo ${#nums[@]}
 echo ${#nums[10]}
 #删除数组元素
--- redirect-exec.sh
+++ redirect-exec.sh
@@ -3,7 +3,7 @@
 echo "重定向未发生"
 exec >log.txt
 echo "www."
-echo "dianping.com"
+echo "xxxxxxxx.com"
 exec >&2
 echo "重定向已恢复"
 cat log.txt
